bcff47ebaea1c510c291debccaf93647cca576b7
avijitsaha118 <sahaavijit908@gmail.com>
2022-04-09 07:55:43 +0600

    if not login redirected added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Components/Home/Home';
@@ -6,6 +5,8 @@ import Login from './Components/Login/Login';
 import Header from './Components/Header/Header';
 import Register from './Components/Register/Register';
 import Products from './Components/Products/Products';
+import Orders from './Components/Orders/Orders';
+import RequreAuth from './Components/RequreAuth/RequreAuth';
 
 function App() {
   return (
@@ -13,9 +14,19 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/products" element={<Products></Products>}></Route>
+
+        <Route path="/products" element={
+          <RequreAuth>
+            <Products></Products>
+          </RequreAuth>
+        }></Route>
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/orders" element={
+          <RequreAuth>
+            <Orders></Orders>
+          </RequreAuth>
+        }></Route>
       </Routes>
 
     </div>
